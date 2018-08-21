95b82c871a173b28be408ba8a3f7078026360fd5
Sam Fullen <sam.fullen@intouchnetworks.com>
2018-08-21 15:45:54 +0100

    Initial commit, index.php and .gitignore

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chhayaben Patel</title>
+    </head>
+    <body>
+        <h1 style="text-align: center">Chhayaben Patel</h1>
+    </body>
+</html>
