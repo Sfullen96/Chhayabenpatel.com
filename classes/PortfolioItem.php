e837bed90fa5de6f2a8c3a2ee1e07df387e166ec
Sam Fullen <sam.fullen@gear4music.com>
2018-09-28 16:51:42 +0100

    Some boilerplate code

---
+++ classes/PortfolioItem.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * A class for an individual Portfolio Item
+ */
+
+class PortfolioItem {
+    public function __construct() {
+    }
+
+    public static function create() {
+
+    }
+
+    public static function update() {
+
+    }
+
+    public function getSingle() {
+
+    }
+
+    public function getAll() {
+
+    }
+
+    public static function delete() {
+
+    }
+}
